947e48a3feb8ce18caaeda54349e7818d2c9329f
Neo <leo1467@yahoo.com.tw>
2023-05-31 20:44:34 +0800

    functions.h新增Ini_st & read_ini

--- functions.h
+++ functions.h
@@ -8,12 +8,21 @@
 #include <iostream>
 #include <sstream>
 #include <string>
+#include <unordered_map>
 #include <vector>
 
 using namespace std;
 using namespace filesystem;
 
-static vector<vector<string>> read_data(path filePath) {
+class Ini_st {
+    public:
+    unordered_map<string* , unordered_map<string *, string *>> keyValue;
+};
+
+[[gnu::always_inline]] 
+inline void read_ini (Ini_st **ini_st, const path filePath) 
+{
+    *ini_st = new Ini_st;
     ifstream infile(filePath);
     vector<vector<string>> data;
     if (!infile) {
@@ -26,9 +35,49 @@ static vector<vector<string>> read_data(path filePath) {
     vector<string> oneRow;
     while (getline(infile, row)) {
         stringstream lineStream(row);
-        while (getline(lineStream, cell, ',')) {
+        while (getline(lineStream, cell, '=')) {
             if (cell != "\r") {
                 if (cell.find('\r') != std::string::npos) {
+                    cell.pop_back();
+                }
+                oneRow.emplace_back(cell);
+            }
+        }
+        if (oneRow.front().front() == '[') 
+        {
+            
+        }
+        data.emplace_back(oneRow);
+        row.clear();
+        cell.clear();
+        oneRow.clear();
+    }
+    infile.close();
+}
+
+[[gnu::always_inline]]
+inline vector<vector<string>> read_data(path filePath)
+{
+    ifstream infile(filePath);
+    vector<vector<string>> data;
+    if (!infile)
+    {
+        cout << filePath << " not found" << endl;
+        exit(1);
+    }
+    //    cout << "reading " << filePath.filename() << endl;
+    string row;
+    string cell;
+    vector<string> oneRow;
+    while (getline(infile, row))
+    {
+        stringstream lineStream(row);
+        while (getline(lineStream, cell, ','))
+        {
+            if (cell != "\r")
+            {
+                if (cell.find('\r') != std::string::npos)
+                {
                     cell.pop_back();
                 }
                 oneRow.push_back(cell);
@@ -43,29 +92,39 @@ static vector<vector<string>> read_data(path filePath) {
     return data;
 }
 
-static vector<path> get_path(path targetPath) {
-    if (!exists(targetPath)) {
+[[gnu::always_inline]]
+inline vector<path> get_path(path targetPath)
+{
+    if (!exists(targetPath))
+    {
         cout << targetPath << " does not exist" << endl;
         exit(1);
     }
     vector<path> filePath;
     copy(directory_iterator(targetPath), directory_iterator(), back_inserter(filePath));
     sort(filePath.begin(), filePath.end());
-    for (auto i = filePath.begin(); i != filePath.end(); i++) {
-        if (i->filename() == ".DS_Store") {
+    for (auto i = filePath.begin(); i != filePath.end(); i++)
+    {
+        if (i->filename() == ".DS_Store")
+        {
             filePath.erase(i);
         }
     }
     return filePath;
 }
 
-static bool is_double(const string &s) {
+[[gnu::always_inline]]
+inline bool is_double(const string &s)
+{
     string::const_iterator it = s.begin();
     int dotsCnt = 0;
-    while (it != s.end() && (isdigit(*it) || *it == '.')) {
-        if (*it == '.') {
+    while (it != s.end() && (isdigit(*it) || *it == '.'))
+    {
+        if (*it == '.')
+        {
             ++dotsCnt;
-            if (dotsCnt > 1 || dotsCnt == s.size()) {
+            if (dotsCnt > 1 || dotsCnt == s.size())
+            {
                 break;
             }
         }
@@ -74,15 +133,21 @@ static bool is_double(const string &s) {
     return !s.empty() && it == s.end();
 }
 
-static string set_precision(const double inputDouble, const int n = 10) {
+[[gnu::always_inline]]
+inline string set_precision(const double inputDouble, const int n = 10)
+{
     stringstream ss;
     ss << fixed << setprecision(n) << inputDouble;
     return ss.str();
 }
 
-static vector<int> find_train_and_test_len(string window, char &delimiter) {
-    for (int i = 0; i < window.length(); i++) {
-        if (isalpha(window[i])) {
+[[gnu::always_inline]]
+inline vector<int> find_train_and_test_len(string window, char &delimiter)
+{
+    for (int i = 0; i < window.length(); i++)
+    {
+        if (isalpha(window[i]))
+        {
             delimiter = window[i];
             break;
         }
@@ -90,52 +155,68 @@ static vector<int> find_train_and_test_len(string window, char &delimiter) {
     string segment;
     vector<string> segmentList;
     stringstream toCut(window);
-    while (getline(toCut, segment, delimiter)) {
+    while (getline(toCut, segment, delimiter))
+    {
         segmentList.push_back(segment);
     }
     vector<int> TrainTest;
-    for (auto i : segmentList) {
-        if (i.length() == 0) {
+    for (auto i : segmentList)
+    {
+        if (i.length() == 0)
+        {
             TrainTest.push_back(0);
         }
-        else {
+        else
+        {
             TrainTest.push_back(stoi(i));
         }
     }
     return TrainTest;
 }
 
-static vector<string> cut_string(string input, char delimiter = ' ') {
+[[gnu::always_inline]]
+inline vector<string> cut_string(string input, char delimiter = ' ')
+{
     string segment;
     vector<string> segmentList;
     stringstream toCut(input);
-    while (getline(toCut, segment, delimiter)) {
+    while (getline(toCut, segment, delimiter))
+    {
         segmentList.push_back(segment);
     }
     return segmentList;
 }
 
-static void check_startRowSize_endRowSize(int startRowSize, int endRowSize) {
-    if (startRowSize > endRowSize) {
+[[gnu::always_inline]]
+inline void check_startRowSize_endRowSize(int startRowSize, int endRowSize)
+{
+    if (startRowSize > endRowSize)
+    {
         cout << "startRowSize > endRowSize" << endl;
         exit(1);
     }
-    else if (startRowSize < endRowSize) {
+    else if (startRowSize < endRowSize)
+    {
         cout << "startRowSize < endRowSize" << endl;
         exit(1);
     }
 }
 
-static vector<int> save_startRow_EndRow(vector<int> &startRow, vector<int> &endRow) {
+[[gnu::always_inline]]
+inline vector<int> save_startRow_EndRow(vector<int> &startRow, vector<int> &endRow)
+{
     vector<int> interval;
-    for (int i = 0; i < startRow.size(); i++) {
+    for (int i = 0; i < startRow.size(); i++)
+    {
         interval.push_back(startRow[i]);
         interval.push_back(endRow[i]);
     }
     return interval;
 }
 
-static int cal_weekday(string date) {
+[[gnu::always_inline]]
+inline int cal_weekday(string date)
+{
     int y = stoi(date.substr(0, 4));
     int m = stoi(date.substr(5, 2)) - 1;
     int d = stoi(date.substr(8, 2));
@@ -145,10 +226,12 @@ static int cal_weekday(string date) {
     return time_out->tm_wday;
 }
 
-static bool is_over_7_days(string date1, string date2) {
+[[gnu::always_inline]]
+inline bool is_over_7_days(string date1, string date2)
+{
     tm d1 = {0, 0, 0, stoi(date1.substr(8, 2)), stoi(date1.substr(5, 2)) - 1, stoi(date1.substr(0, 4)) - 1990};
     tm d2 = {0, 0, 0, stoi(date2.substr(8, 2)), stoi(date2.substr(5, 2)) - 1, stoi(date2.substr(0, 4)) - 1990};
-    
+
     time_t t1 = mktime(&d1);
     time_t t2 = mktime(&d2);
     double daysApart = abs(difftime(t1, t2) / (60.0 * 60.0 * 24.0));
@@ -156,61 +239,82 @@ static bool is_over_7_days(string date1, string date2) {
     return daysApart >= 7;
 }
 
-//static bool is_week_changed(vector<string> &date, int bigWeekDay, int smallWeekDay, int big_i, int small_i) {
-//    int bigDate = stoi(date[big_i].substr(8, 2));
-//    int smallDate = stoi(date[small_i].substr(8, 2));
-//    return (bigWeekDay <= smallWeekDay ||
-//            bigDate - smallDate >= 7 ||
-//            ((bigDate < smallDate) && (bigDate + 30 - smallDate) >= 7));
-//}
+// bool is_week_changed(vector<string> &date, int bigWeekDay, int smallWeekDay, int big_i, int small_i) {
+//     int bigDate = stoi(date[big_i].substr(8, 2));
+//     int smallDate = stoi(date[small_i].substr(8, 2));
+//     return (bigWeekDay <= smallWeekDay ||
+//             bigDate - smallDate >= 7 ||
+//             ((bigDate < smallDate) && (bigDate + 30 - smallDate) >= 7));
+// }
 
-static int find_index_of_string_in_vec(const vector<string> &stringVector, const string targetString) {
+[[gnu::always_inline]]
+inline int find_index_of_string_in_vec(const vector<string> &stringVector, const string targetString)
+{
     auto iter = find(stringVector.begin(), stringVector.end(), targetString);
-    if (iter == stringVector.end()) {
+    if (iter == stringVector.end())
+    {
         cout << "cant find " << targetString << endl;
         exit(1);
     }
     return (int)distance(stringVector.begin(), iter);
 }
 
-static vector<string> set_certain_range_of_vec(const string &inputString, vector<string> &targetVector) {
-    if (inputString.find(",") != string::npos) {
+[[gnu::always_inline]]
+inline vector<string> set_certain_range_of_vec(const string &inputString, vector<string> &targetVector)
+{
+    if (inputString.find(",") != string::npos)
+    {
         return cut_string(inputString, ',');
     }
-    string inputForSetTarget = [&]() {
-        if (inputString == "all") {
+    string inputForSetTarget = [&]()
+    {
+        if (inputString == "all")
+        {
             return targetVector.front() + " to " + targetVector.back();
         }
         return inputString;
     }();
     vector<string> setTarget = cut_string(inputForSetTarget);
-    if (auto iter = find_if(setTarget.begin(), setTarget.end(), [](string i) { return i == "to"; }); iter != setTarget.end()) {
+    if (auto iter = find_if(setTarget.begin(), setTarget.end(), [](string i) { return i == "to"; }); iter != setTarget.end())
+    {
         setTarget.erase(iter);
     }
-    auto find_target = [&](string &targetString) { return find_if(targetVector.begin(), targetVector.end(), [&](string &string) { return string == targetString; }); };
+    auto find_target = [&](string &targetString)
+    { 
+        return find_if(targetVector.begin(), targetVector.end(), [&](string &string) { return string == targetString; }); 
+    };
     auto firstWindowIter = find_target(setTarget.front());
     auto lastWindowIter = find_target(setTarget.back()) + 1;
-    if (firstWindowIter == targetVector.end() || lastWindowIter - 1 == targetVector.end()) {
+    if (firstWindowIter == targetVector.end() || lastWindowIter - 1 == targetVector.end())
+    {
         cout << "cant find inpuString in targetVector" << endl;
         exit(1);
     }
     return vector<string>(firstWindowIter, lastWindowIter);
 }
 
-static string remove_zeros_at_end(double n) {
+[[gnu::always_inline]]
+inline string remove_zeros_at_end(double n)
+{
     string stringWOzeroAtEnd = set_precision(n);
     stringWOzeroAtEnd.erase(stringWOzeroAtEnd.find_last_not_of('0') + 1, std::string::npos);
     return stringWOzeroAtEnd;
 }
 
-static string get_date(vector<string> &date, int startRow, int endRow) {
+[[gnu::always_inline]]
+inline string get_date(vector<string> &date, int startRow, int endRow)
+{
     return date[startRow] + "_" + date[endRow];
 }
 
-vector<int> change_vec_string_to_int(vector<string> aStrategyStrings) {
-    if (aStrategyStrings.size() != 0) {
+[[gnu::always_inline]]
+inline vector<int> change_vec_string_to_int(vector<string> aStrategyStrings)
+{
+    if (aStrategyStrings.size() != 0)
+    {
         vector<int> aStrategyInt;
-        for (auto s : aStrategyStrings) {
+        for (auto s : aStrategyStrings)
+        {
             aStrategyInt.push_back(stoi(s));
         }
         return aStrategyInt;
